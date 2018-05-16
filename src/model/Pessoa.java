9dcd05e836e271e280d09ef3719db17f5586e05e
eduardo.duarte <eduardo.duarte@UNI-MKT-CHF9LZ1.grupouniesp.com.br>
2018-05-16 11:48:29 -0300

    Alteração geral nos arquivos.

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -5,47 +5,60 @@ public class Pessoa {
 	String cpf;
 	String rg;
 	String genero;
-	
-//	Construtor padrao
-	public Pessoa() {
-		
-	}
-	
-//	Construtor crianca
-	public Pessoa(String nome, String rg){
+
+	/**
+	 * Descrição: Construtor destinado para crianças.
+	 * 
+	 * @param nome
+	 * @param rg
+	 */
+	public Pessoa(String nome, String rg) {
 		this.nome = nome;
 		this.rg = rg;
 		this.cpf = "Não há";
 	}
-	
-//	Construtor adulto
+
+	/**
+	 * Descrição: Construtor destinado para adultos.
+	 * 
+	 * @param nome
+	 * @param rg
+	 * @param cpf
+	 */
 	public Pessoa(String nome, String rg, String cpf) {
 		this.nome = nome;
 		this.rg = rg;
 		this.cpf = cpf;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getRg() {
 		return rg;
 	}
+
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
+
 	public String getGenero() {
 		return genero;
 	}
+
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
--- src/view/Executa.java
+++ src/view/Executa.java
@@ -1,13 +1,13 @@
-package run;
+package view;
 
 import model.Pessoa;
 
 public class Executa {
 	public static void main(String[] args) {
-		
-		Pessoa crianca = new Pessoa("Joao", "36.495.444-X");
+
+		Pessoa crianca = new Pessoa("João", "36.495.444-X");
 		Pessoa adulto = new Pessoa("Maria", "36.495.444-4", "433.760.268-23");
-		
+
 		System.out.println("Nome: " + crianca.getNome());
 		System.out.println("RG: " + crianca.getRg());
 		System.out.println("CPF: " + crianca.getCpf());
@@ -16,9 +16,9 @@ public class Executa {
 		System.out.println("RG: " + adulto.getRg());
 		System.out.println("CPF: " + adulto.getCpf());
 		System.out.println("--");
-		System.out.println("A pessoa deixou de ser crianca");
+		System.out.println("A pessoa não é mais criança");
 		crianca.setCpf("455.750.285-23");
 		System.out.println("CPF: " + crianca.getCpf());
-		
+
 	}
 }
